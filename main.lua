2fae5f0761854b552d3e3d91a242c6ed213c20d9
javck <javck.zack@gmail.com>
2015-08-10 12:01:37 +0800

    追加新內容

--- main.lua
+++ main.lua
@@ -20,6 +20,11 @@ print(#t)
 --table的記憶體地址
 print(tostring(t))
 
+--利用ipairs印出所有Table的index和value
+for i,v in pairs(t) do 
+	print(i .. '->' .. v)
+end
+
 --宣告一個Hash形式的Table，以下兩種方式結果相同
 local hash = {
 	x = "降龍十八掌",
@@ -28,6 +33,11 @@ local hash = {
 local hash = {}
 hash.x = "降龍十八掌"
 hash.y = "六脈神劍"
+
+--利用pairs印出所有Table的key和value
+for key,value in pairs(hash) do 
+	print(key .. '->' .. value)
+end
 
 --取用Hash Table的作法
 print(hash["x"])
@@ -48,3 +58,21 @@ print( "新角色".. t.player.name .. "進入房間,配戴" .. t.weapon[2] .. ",
 
 --當Table用不到時，可以設為nil以作記憶體管理
 t = nil
+
+--Table的物件導向
+local player = {
+	--宣告物件屬性
+	hp = 100,
+	mp = 50,
+	name = "江湖小蝦米",	
+}
+--宣告物件行為
+player.greet = function()
+	print(player.name .. '向英雄敬禮!')
+end
+player.attack = function(enmy)
+	print(player.name .. '攻擊' .. enmy)
+end
+
+player.greet()
+player.attack('史萊姆')
